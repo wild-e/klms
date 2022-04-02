66c0219da923a48211402cf52fd4e77b5157f0b6
wild-e <wild.emmanuelle@gmail.com>
2022-04-02 17:43:04 +0200

    added fontawesome and color config

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- import styles to be used
+
 
 function App() {
   return (
 	<div>
-		<div className="flex flex-col w-full h-screen bg-sky-700">
-			<h1 className="justify-start text-6xl text-neutral-50 font-black p-4">KLMS</h1>
-			<div className='items-center space-y-6 text-center my-auto text-neutral-50 font-bold text-4xl'>
+		<div className="flex flex-col w-full h-screen bg-gradient-to-r from-blue-sapphire to-blue-munsell">
+			<h1 className="justify-start text-6xl text-beige-lace font-black p-4">KLMS</h1>
+			<div className='items-center tracking-wide leading-relaxed space-y-14 text-center my-auto text-beige-lace font-bold text-4xl'>
 				<p>
 					Votre professeur de math, physique, chimie
 					<br/>
 					de la primaire au lycée
+					<FontAwesomeIcon icon={solid('school')} className="ml-2"/>
 					<br/>
 					sur Saverne et environs
 				</p>
-				<button className='p-3 font-black rounded-lg border-b border-r border-color-sky-900 bg-blue-300 uppercase' type="button">
+				<button 
+					className='p-3 font-black font-inter rounded-lg border-b-4 border-l-2 hover:border-b-0 hover:border-l-0 hover:border-t-4 hover:border-r-2  border-red-persimmon-darker bg-red-persimmon uppercase shadow-xl' 
+					type="button"
+				>
 					Contactez-moi
 				</button>
 			</div>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,7 +3,48 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'crimson': ['Crimson pro'], // ex: class="font-crimson"
+        'inter': ['Inter'],
+      },
+      colors: {
+          blue: {
+            munsell: '#0091AD',
+            sapphire: '#165F78',
+            cadet: '#2B2D42',
+            eerie: '#191516',
+          },
+          gray: {
+            granite: '#605F6B',
+            silver: '#CAC3BC',
+          },
+          beige: {
+            lace: '#FFF4E4',
+          },
+          red: {
+            persimmon: {
+              DEFAULT: '#E55812',
+              lighter: '#F07C42',
+              darker: '#BD490F'
+            },
+            amaranth: {
+              DEFAULT: '#D90429',
+              lighter: '#F0052C',
+              darker: '#C80425'
+            },
+          },
+          brown: {
+            golden: '#99621E',
+          },
+          yellow: {
+            lemon: '#FCFC62',
+          },
+          green: {
+            lime: '#6BD425'
+          }
+      },
+    },
   },
   plugins: [],
 }
